2cb4b1831533b0a2f53994cb153c29b4b849b48a
HP <efafafaef>
2020-07-15 19:21:44 +0300

    feat: improve script

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 // Task 1
 
-let userName = prompt('Приветствую, о незнакомец(ка). Каково твоё благородное имя?');
+let greeting = 'Приветствую, о незнакомец(ка). Каково твоё благородное имя?';
+let questionWhatsUp = 'Всё ли благополучно в твоих землях';
+let questionMeal = 'Что ты вкушал(а) на обед';
+let answerPositive = 'Сколь велико удовольствие от встречи с собратом';
+let answerNegative = 'К моему огорчению, не могу разделить удовольствие от совместной трапезы, ибо я робот и человеческие вкусы мне не близки. Прощай!';
 
-prompt(`Всё ли благополучно в твоих землях, ${userName}?`);
+let dialogAll = `a. — ${greeting}`;
+let userName = prompt(greeting);
+dialogAll += '\n' + 'b. — ' + userName;
 
-let userDinner = prompt(`Что ты вкушал(а) на обед, о ${userName}?`);
+let dialogPhrase = `${questionWhatsUp}, ${userName}?`;
+dialogAll += '\n' + 'c. — ' + dialogPhrase;
+let userMood = prompt(dialogPhrase);
+dialogAll += '\n' + 'd. — ' + userMood;
 
-if (userDinner.indexOf('Электричество') != -1 || userDinner.indexOf('электричество') != -1 || userDinner.indexOf('Моторное масло') != -1 || userDinner.indexOf('моторное масло') != -1){
-    prompt (`Сколь велико удовольствие от встречи с собратом, о ${userName}!`);
+dialogPhrase = `${questionMeal}, ${userName}?`;
+dialogAll += '\n' + 'e. — ' + dialogPhrase;
+let userDinner = prompt(dialogPhrase);
+dialogAll += '\n' + 'f. — ' + userDinner;
+
+let userGoodbye;
+
+if (userDinner.toLowerCase().indexOf('электричество') != -1 || userDinner.toLowerCase().indexOf('моторное масло') != -1) {
+    dialogPhrase = `${answerPositive}, о ${userName}!`;
+    dialogAll += '\n' + 'g. — ' +  dialogPhrase;
+    userGoodbye = prompt (dialogPhrase);
 } else {
-    prompt('К моему огорчению, не могу разделить удовольствие от совместной трапезы, ибо я робот и человеческие вкусы мне не близки. Прощай!');
+    dialogAll += '\n' + 'g. — ' +  answerNegative;
+    userGoodbye = prompt(answerNegative);
 }
+dialogAll += '\n' + 'h. — ' + userGoodbye; 
+
+console.log(dialogAll);
 
 // Task 2
 
 let firstUserNumber = +prompt('Enter first number, please');
 let secondUserNumber = +prompt('Enter second number, please');
 
-for (let i = firstUserNumber; i <= secondUserNumber; i++) {
-    if (i % 2 === 0 || i % 5 === 0) {
-        console.log(i);
-    }
+if (firstUserNumber > secondUserNumber) {
+    secondUserNumber = +prompt('Wrong format. The second number should be greater than the first number. Please enter other second number');
 }
 
-for (let i = firstUserNumber; i >= secondUserNumber; i--) {
+for (let i = firstUserNumber; i <= secondUserNumber; i++) {
     if (i % 2 === 0 || i % 5 === 0) {
         console.log(i);
     }
